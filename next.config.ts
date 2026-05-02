34a1b163b54dad874919918beb415df6919bca30
alsarudev <alfonso.sanchez.ruizz@gmail.com>
2026-05-02 12:54:35 +0200

    Delete page register

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,15 @@ const nextConfig: NextConfig = {
     NEXT_BOXING_AWR_KEY: process.env.NEXT_BOXING_AWR_KEY || "",
   },
   logging: false,
+  async redirects() {
+    return [
+      {
+        source: "/register",
+        destination: "/login",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
